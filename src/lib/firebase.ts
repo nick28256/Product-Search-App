53de7b02db219bbe44daddafbdac677b0ecf32d5
nick28256 <serban.nicolae.n3z@student.ucv.ro>
2025-06-16 14:40:38 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD072MDKKtXrJO6dy6UluN79qJx5TBrI7I",
-    authDomain: "expotest-efa5a.firebaseapp.com",
-    projectId: "expotest-efa5a",
-    storageBucket: "expotest-efa5a.firebasestorage.app",
-    messagingSenderId: "830164299556",
-    appId: "1:830164299556:web:38717b621d877d08940db1",
-    measurementId: "G-6QXET601EJ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
